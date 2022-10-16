a89c20f5b0ec63ab124d015f47d630bf6c9d815f
david alvarez <david@southfloridaathleticclub.com>
2022-10-16 00:16:49 -0400

    working on scroll rate issue

--- utils.py
+++ utils.py
@@ -2,17 +2,28 @@ import os
 import subprocess
 from mutagen.mp3 import MP3
 from subprocess import STDOUT, check_output
+from wand.image import Image
 
+def get_image_height(image_path):
+    with Image(filename=image_path) as i:
+        return i.height
 
 def create_scrolling_video(image_output_path, video_output_path, silent_video_output_path, audio_input_path, final_video_output_path):
     audio_length = MP3(audio_input_path).info.length
+    scroll_rate = 0
+    
+    if audio_length < 25:
+        scroll_rate = 0.02
+    else:
+        image_height = get_image_height(image_output_path)
+        scroll_rate = audio_length / image_height
 
     try:
         # Create Silent Video W/ Scrolling
         subprocess.run(f"""
-                ffmpeg -f lavfi -i color=s=1920x1080:color=white -loop 1 -t {audio_length} \
+                ffmpeg -f lavfi -i color=s=1920x1080:color=white -loop 1 -t {scroll_rate} \
                 -i {image_output_path} -filter_complex \
-                "[1:v]scale=1920:-2,setpts=if(eq(N\,0)\,0\,1+{audio_length}/TB),fps=25[fg]; \
+                "[1:v]scale=1920:-2,setpts=if(eq(N\,0)\,0\,1+{scroll_rate}/TB),fps=25[fg]; \
                 [0:v][fg]overlay=y=-'t*h*0.02':eof_action=endall[v]" -map "[v]" {video_output_path}
             """, shell=True, check=True, text=True)
 
