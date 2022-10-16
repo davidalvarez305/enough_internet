e7a81ef343c5222cbd88a99b9973bd159b430194
david alvarez <david@southfloridaathleticclub.com>
2022-10-16 15:00:09 -0400

    trying to figure out scroll rate

--- utils.py
+++ utils.py
@@ -10,21 +10,16 @@ def get_image_height(image_path):
 
 def create_scrolling_video(image_output_path, video_output_path, silent_video_output_path, audio_input_path, final_video_output_path):
     audio_length = MP3(audio_input_path).info.length
-    scroll_rate = 0
-    
-    if audio_length < 25:
-        scroll_rate = 0.02
-    else:
-        image_height = get_image_height(image_output_path)
-        scroll_rate = audio_length / image_height
+    image_height = get_image_height(image_output_path) * 20
+    scroll_rate = audio_length / image_height
 
     try:
         # Create Silent Video W/ Scrolling
         subprocess.run(f"""
-                ffmpeg -f lavfi -i color=s=1920x1080:color=white -loop 1 -t {scroll_rate} \
+                ffmpeg -f lavfi -i color=s=1920x1080:color=white -loop 1 -t {audio_length} \
                 -i {image_output_path} -filter_complex \
-                "[1:v]scale=1920:-2,setpts=if(eq(N\,0)\,0\,1+{scroll_rate}/TB),fps=25[fg]; \
-                [0:v][fg]overlay=y=-'t*h*0.02':eof_action=endall[v]" -map "[v]" {video_output_path}
+                "[1:v]scale=1920:-2,setpts=if(eq(N\,0)\,0\,1+{audio_length}/TB),fps=25[fg]; \
+                [0:v][fg]overlay=y=-'t*h*{scroll_rate}':eof_action=endall[v]" -map "[v]" {video_output_path}
             """, shell=True, check=True, text=True)
 
         # Add audio to silent video
